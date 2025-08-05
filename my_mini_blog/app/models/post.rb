b479273438fdaf8e1054e876d19a10d44bbb9e77
NuclearJuno <junolcontato@gmail.com>
2025-08-05 18:40:19 +0000

    Atualização mini blog

--- my_mini_blog/app/models/post.rb
+++ my_mini_blog/app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+  validates :title, length: { maximum: 20,
+    too_long: "%{count} characters is the maximum allowed", minimum: 5,
+    too_short: "%{count} characters is the minimum allowed" }
+ validates :description, length: { maximum: 2000,
+   too_long: "%{count} characters is the maximum allowed", minimum: 100,
+   too_short: "%{count} characters is the minimum allowed" }
+  validates :description, presence: true
 end
